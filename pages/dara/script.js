ec8cb82ad1067add69880b5c92b67cf5b382ebed
darinhaa <hiandara.wosnhaki@escola.pr.gov.br>
2025-06-13 11:17:40 -0300

    alterei o trabalho

--- pages/dara/script.js
+++ pages/dara/script.js
@@ -1,1 +1,25 @@
-// pagina do artur
+function togglePassword() {
+    const senha = document.getElementById("senha");
+    const toggle = document.getElementById("toggleSenha");
+  
+    if (senha.type === "password") {
+      senha.type = "text";
+      toggle.textContent = "🙈";
+    } else {
+      senha.type = "password";
+      toggle.textContent = "👁";
+    }
+  }
+  
+  function handleLogin(event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+  
+    if (email === "admin" && senha === "1234") {
+      alert("Login bem-sucedido!");
+    } else {
+      alert("E-mail ou senha incorretos.");
+    }
+  }
+  
